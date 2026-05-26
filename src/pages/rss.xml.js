444b55131a3c8412a676e8970c8930345beacc8f
JAE <twonedec@gmail.com>
2026-05-26 23:25:43 +0900

    draft: 레이더 글 8개 비공개 처리 + skhynix 개발검증 글로 재작성

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-  const posts = await getCollection('posts');
+  const posts = await getCollection('posts', ({ data }) => !data.draft);
   const sorted = posts.sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
 
   return rss({
